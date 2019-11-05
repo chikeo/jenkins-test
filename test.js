4e283a54dbf361839b89bf54d906164880e6d84e
Chikelue Oji <chike_oji@yahoo.com>
2019-11-05 11:07:56 +0100

    Modified test group name in describes

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 
 // Trigger Jenkins Job for Github PR
 // This is a test
-describe('TEST', () => {
+describe('TEST1', () => {
   it('passed', () => {
     expect(true).to.be.true;
   });
